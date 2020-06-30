affd4a45f013b57dec31d406a1472616611aa95c
huangjun <keithhuang0124@gmail.com>
2020-07-01 06:32:00 +0800

    Add asp net mvc site

---
+++ Utilities/ExceptionExtend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public static class ExceptionExtend
+    {
+        public static string GetMessage(this Exception e)
+        {
+            StringBuilder sb = new StringBuilder();
+            GetMessage(sb, e);
+            return sb.ToString();
+        }
+
+        private static void GetMessage(StringBuilder sb, Exception e)
+        {
+            sb.AppendLine(e.Message);
+            sb.AppendLine(e.StackTrace);
+            if (e.InnerException != null)
+            {
+                GetMessage(sb, e.InnerException);
+            }
+        }
+    }
+}
---
+++ WebMvcSite/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Utilities;
+
+namespace WebMvcSite
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ThreadPool.QueueUserWorkItem(x => PingMySelf());
+        }
+
+        private void PingMySelf()
+        {
+            while (true)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                try
+                {
+                    using (var webClient = new WebClient())
+                    {
+                        webClient.DownloadStringTaskAsync("http://localhost:1807").GetAwaiter().GetResult();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.GetMessage());
+                }
+            }
+        }
+    }
+}
