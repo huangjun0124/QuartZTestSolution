d2a53d8ea8fc8e34d5a167c02fdff54d042436e9
huangjun <keithhuang0124@gmail.com>
2020-07-01 06:51:02 +0800

    Add log4net

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 ## Quartz Api 文档
 [Quartz.NET 3.x Tutorial](https://www.quartz-scheduler.net/documentation/quartz-3.x/tutorial/index.html)
 
-
-
+## log4net 集成到 aspnet mvc
+[Configure Log4Net with ASP.NET MVC](https://developerslogblog.wordpress.com/2017/07/30/configure-log4net-with-asp-net-mvc/)
+[log4net 使用总结- （2）在ASP.NET MVC 中使用](https://article.itxueyuan.com/JkPZJ)
+[Use Log4net In ASP.NET MVC Application](https://www.c-sharpcorner.com/article/use-log4net-in-asp-net-mvc-application/)
--- WebMvcSite/Controllers/HomeController.cs
+++ WebMvcSite/Controllers/HomeController.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace WebMvcSite.Controllers
 {
     public class HomeController : Controller
     {
+        private ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public ActionResult Index()
         {
+            Logger.Info("Home:Index visited...");
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            Logger.Info("Home:About visited...");
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            Logger.Info("Home:Contact visited...");
             return View();
         }
     }
